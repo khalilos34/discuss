da9ed36e250cda26c5ff95e03b7a3a8e8a543ed8
khalilos34 <khalil.naoueji@gmail.com>
2024-04-13 20:55:52 +0200

    changing posts to code

--- components/posts/post-show.tsx
+++ components/posts/post-show.tsx
@@ -15,7 +15,7 @@ export default async function PostShow({ postId }: PostShowProps) {
   return (
     <div className="m-4">
       <h1 className="my-2 text-2xl font-bold">{post.title}</h1>
-      <p className="rounded border p-4">{post.content}</p>
+      <code className="rounded border p-4">{post.content}</code>
     </div>
   );
 }
